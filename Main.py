6ec8cda3c75d7f2f53ddf25181b3484e2af1d82b
MTISSOT <metissot@outlook.com>
2020-05-06 22:08:57 -0300

    migrando a objeto

--- Main.py
+++ Main.py
@@ -3,48 +3,51 @@ from persona import *
 from reporte import *
 from so import *
 
-
-# ----- VARIABLES
-opcion = 0
-Logo = '''\n\n       		     ╔════════════════════════════╗ 
+class main():
+	#constructor 
+	def __init__(self):
+		
+		self.opcion = 0
+		self.Logo = '''\n\n       		     ╔════════════════════════════╗ 
                      ║ SISTEMA DE HISORIA CLINICA ║ 
                      ╚════════════════════════════╝ '''
 
-#--------------------- FIN VARIABLES
-print(Logo)
-
-#AltaPer(Dni, Ape, Nom)
-#ConsultarPer()
-def ValidaOpcion():
-	print ('''	\n   |1 - ALTA PERSONA | '''+
-'''|2 - CONSULTAR PERSONAS| |3 - GENERAR REPORTE|  |4 - SALIR| \n''')
-
-	try:
-		opcion = int(input(' OPCION: '))
+	#metodos 
+	def DibujarLogo(self):
+		print(self.Logo)
 		
-		if opcion == 1: # ALTA PERSONAS
-			limpiarConsola()
-			print(Logo)
-			AltaPer(Dni, Ape, Nom)
+	def ValidaOpcion(self):
+		print ('''	\n   |1 - ALTA PERSONA | '''+
+	'''|2 - CONSULTAR PERSONAS| |3 - GENERAR REPORTE|  |4 - SALIR| \n''')
+
+		try:
+			self.opcion = int(input(' OPCION: '))
+			
+			if self.opcion == 1: # ALTA PERSONAS
+				limpiarConsola()
+				Principal.DibujarLogo()
+				AltaPer(Dni, Ape, Nom)
+				Principal.ValidaOpcion()
+			if self.opcion == 2: # CONSULTA PERSONAS
+				limpiarConsola()
+				Principal.DibujarLogo()
+				ConsultarPer()
+				Principal.ValidaOpcion()
+			if self.opcion == 3:	# GENERAR REPORTE
+				limpiarConsola()
+				Principal.DibujarLogo()
+				generarReporte()
+				AbrirPdf()
+				Principal.ValidaOpcion()
+			if self.opcion == 4: # SALIR
+				quit()
+			else: 
+				Principal.ValidaOpcion()
+		except ValueError:
+			
+			print(' formato de datos incorrecto ')
 			ValidaOpcion()
-		if opcion == 2: # CONSULTA PERSONAS
-			limpiarConsola()
-			print(Logo)
-			ConsultarPer()
-			ValidaOpcion()
-		if opcion == 3:	# GENERAR REPORTE
-			limpiarConsola()
-			print(Logo)
-			generarReporte()
-			AbrirPdf()
-			ValidaOpcion()
-		if opcion == 4: # SALIR
-			quit()
-		else: 
-			ValidaOpcion()
-	except ValueError:
-		
-		print(' formato de datos incorrecto ')
-		ValidaOpcion()
-ValidaOpcion()
+Principal = main()
+Principal.DibujarLogo()
+Principal.ValidaOpcion()
 
